0dafd05c941e21b94277d438b4addff2ba2c996a
AlkanSte <49679645+AlkanSte@users.noreply.github.com>
2020-02-26 11:45:30 +0300

    Update LogReg_GD_QWK_v1.py

--- LogReg_GD_QWK_v1.py
+++ LogReg_GD_QWK_v1.py
@@ -88,7 +88,32 @@ from sklearn import datasets
 bc = datasets.load_breast_cancer()
 X,y = bc.data, bc.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 142)
+#Now lets test our algo!
+
+#Data shuffling primarily for train_test_split function
+def shuffle_data(X, y, seed=None):
+    """ Random shuffle of the samples in X and y """
+    if seed:
+        np.random.seed(seed)
+    idx = np.arange(X.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
+
+#train_test_split function that mimics sklearn function
+def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
+    """ Split the data into train and test sets """
+    if shuffle:
+        X, y = shuffle_data(X, y, seed)
+    # Split the training data from test data in the ratio specified in
+    # test_size
+    split_i = len(y) - int(len(y) // (1 / test_size))
+    X_train, X_test = X[:split_i], X[split_i:]
+    y_train, y_test = y[:split_i], y[split_i:]
+
+    return X_train, X_test, y_train, y_test
+
+#Run the train test plit
+train_test_split(X, y, test_size=0.7, shuffle=True, seed=142):
 
 #Run the regression
 regressor = LogisticRegression(learning_rate = 0.001, n_iters = 1000)
